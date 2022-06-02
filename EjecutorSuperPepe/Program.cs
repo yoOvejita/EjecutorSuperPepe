ccb9cd8d6a662c66ed7f70c68e68a354feb103cc
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2022-06-01 21:31:46 -0400

    Agregar archivos de proyecto.

---
+++ EjecutorSuperPepe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LibreriaCompiladorPepe;
+
+namespace EjecutorSuperPepe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pepematic maquina = new Pepematic(args[0]);
+            maquina.Ejecutar();
+        }
+    }
+}
